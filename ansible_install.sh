e295d313334bf2f9544b2d51b5366b6fd9c09023
Manoharpatil-Devops <manohar143patils@gmail.com>
2025-07-09 11:49:37 +0530

    Update ansible_install.sh

--- ansible_install.sh
+++ ansible_install.sh
@@ -10,5 +10,11 @@ ssh-keygen
 echo "Key Generated"
 echo "Please set root user password"
 passwd root
-echo "Changing the permission of the ssh"
-vim /etc/ssh/sshd
+echo "===Changing the permissions of the sshd===="
+cd /etc/ssh/
+sed -i '40 s/^#PermitRootLogin prohibit-password/PermitRootLogin yes/' sshd_config
+sed -i '65 s/^PasswordAuthentication no/PasswordAuthentication yes/' sshd_config
+echo"===Permissions are changed and now restaring the sshd==="
+systemctl restart sshd
+echo"===Restart is completed===
+
